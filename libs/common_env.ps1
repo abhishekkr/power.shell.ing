a53d29513cb44161112a2df6ec512dc478b964ac
abhishekkr <abhikumar163@gmail.com>
2013-01-26 19:11:38 +0530

    CommonEnv currently just

---
+++ libs/common_env.ps1
@@ -0,0 +1,12 @@
+# Common Env
+###############################################################################
+# Defaults
+
+# need pre-set $root, $home_dir_name
+$home_dir= $root + $home_dir_name
+$download_dir_name = "Downloads"
+$download_dir = $home_dir + "\" + $download_dir_name
+$app_dir_name = "Applications"
+$app_dir = $home_dir + "\" + $app_dir_name
+
+###############################################################################
